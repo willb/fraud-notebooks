64687de3c6ad363f576ffd2306fdecc0f0d045d9
William Benton <self@willbenton.com>
2020-04-17 17:05:07 -0500

    saving intermediate pipeline stages

--- mlworkflows/util.py
+++ mlworkflows/util.py
@@ -5,6 +5,7 @@ import cloudpickle as cp
 
 def serialize_to(obj, default_filename):
     filename = os.getenv("S2I_PIPELINE_STAGE_SAVE_FILE", default_filename)
+    cp.dump(obj, open(default_filename, "wb"))
     cp.dump(obj, open(filename, "wb"))
 
 def sample_corresponding(count, source, *sources, **kwargs):
